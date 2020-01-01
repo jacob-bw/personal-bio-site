deb3d576c4b1ca9518e57ead93af4f5e24e40a56
Jacob <wittenberg.jacob@gmail.com>
2020-01-01 00:51:12 -0600

    cards are printing. sites and githubs are linked. gotta deploy a few more projects and add to seed data, but functionally, projects component is done. stylistically, like 85%

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -8,21 +8,15 @@ import './projects.scss';
 const projectCard = (projectData) => {
   let domString = '';
   domString += `
-  <div class="project-card col-md-3">
+  <div class="project-card col-md">
     <div class="card-body">
-      <div class="border-top my-3 hide"></div>
-      <div class="view overlay">
-      <img class="card-img-top" src="${projectData.projectImgUrl}" alt="Card image cap">
-      <a href="#!">
-        <div class="mask rgba-white-slight"></div>
-      </a>
-    </div>
-        <h4 class="card-title" id="${projectData.id}">${projectData.projectName}</h4>
-        <h6 id="">${projectData.projectSummary}</h6>
-        <div class="project-footer d-flex justify-content-between">
+    <div class="crop-image"><img src=${projectData.projectImgUrl} class="card-img-top"></div>
+      <h4 class="card-title" id="${projectData.id}">${projectData.projectName}</h4>
+      <h6 id="">${projectData.projectSummary}</h6>
+      <div class="project-footer d-flex justify-content-around">
         <li id="icons"><a href="${projectData.projectGitHub}" target="_blank"><i class="fab fa-github"></i></a></li>
         <li id="icons"><a href="${projectData.projectUrl}" target="_blank"><i class="fas fa-desktop"></i></a></li>    
-        </div>
+      </div>
     </div>
   </div>
   `;
@@ -39,11 +33,12 @@ const printProjects = () => {
   pd.getProjectsData()
     .then((projects) => {
       let domString = '';
-      domString += '<div class="container">';
+      domString += '<div class="project-container">';
+      domString += '<div class="row">';
       projects.forEach((project) => {
         domString += projectCard(project);
       });
-      domString += '</div>';
+      domString += '</div></div>';
       utilities.printToDom(domString, 'projectsPage');
     })
     .catch((errorFromPrintProjects) => console.error(errorFromPrintProjects));
